bc325ee76b7bbb32e58d3d857940953cd9531c8c
李一贤 <573096385@qq.com>
2018-07-13 16:04:46 +0800

    Initial Commit

---
+++ ImageConverBase64/ImageConverBase64.h
@@ -0,0 +1,19 @@
+//
+//  ImageConverBase64.h
+//  ImageConverBase64
+//
+//  Created by 李一贤 on 2018/7/13.
+//  Copyright © 2018年 李一贤. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageConverBase64.
+FOUNDATION_EXPORT double ImageConverBase64VersionNumber;
+
+//! Project version string for ImageConverBase64.
+FOUNDATION_EXPORT const unsigned char ImageConverBase64VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageConverBase64/PublicHeader.h>
+
+
